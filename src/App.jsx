042d565ab0eb50741a7094d75a285efb96c9213c
Zulqarnain <zulqarnainchohan250@gmail.com>
2025-01-02 20:31:28 -0800

    added scorers data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Scorers from './components/Scorers'
 import("./App.css")
 const App = () => {
   return (
@@ -8,7 +9,7 @@ const App = () => {
     <BrowserRouter>
          <Navbar/> 
       <Routes>
-         
+         <Route path = "/scorers" element = {<Scorers/>}/>
          
       </Routes> 
     </BrowserRouter>
